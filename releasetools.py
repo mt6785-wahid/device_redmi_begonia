bec4dead247c67e84714dc7e25909a1d8ee59eda
Wahid Khan <wahidzk0091@gmail.com>
2023-12-29 14:03:21 +0000

    Begonia: update releasetools
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -17,15 +17,32 @@
 import common
 import re
 
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
 def AddImage(info, dir, basename, dest):
-  name = basename
-  data = info.input_zip.read(dir + "/" + basename)
-  common.ZipWriteStr(info.output_zip, name, data)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+  path = dir + "/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def FullOTA_InstallBegin(info):
   AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
   info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
   info.script.AppendExtra('set_metadata("/tmp/flash_super_dummy.sh", "uid", 0, "gid", 0, "mode", 0755);')
   info.script.AppendExtra('run_program("/tmp/flash_super_dummy.sh");')
+  return
+
+def OTA_InstallEnd(info):
+  info.script.Print("Patching dtbo and vbmeta images...")
+  AddImage(info, "IMAGES", "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo")
+  AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta")
   return
