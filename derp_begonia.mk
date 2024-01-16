380d9563bf7c974a3833d1828680f5b0c3bdc355
Wahid Khan <wahidzk0091@gmail.com>
2024-01-16 18:20:26 +0000

    Revert "Begonia: Stop shipping 32-bit Zygote"
    We still use alot of apps which are based on 32-bit zygote, so let's revert this.
    This reverts commit d0a225adb15f07b960dca1c1e98431da8fed2299.

--- derp_begonia.mk
+++ derp_begonia.mk
@@ -8,7 +8,7 @@
 PRODUCT_RELEASE_NAME := begonia
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
