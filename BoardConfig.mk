66f53416fc5fc4c42e375b06a90dd26191ba1068
idoybh <idoybh2@gmail.com>
2024-06-29 22:24:48 +0530

    begonia: BoardConfig: Conditionally enable AVB
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,14 @@ TARGET_OTA_ASSERT_DEVICE := begonia
 
 # Avb
 BOARD_AVB_ENABLE := true
+ifneq (user,$(TARGET_BUILD_VARIANT))
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+else ifneq (,$(wildcard .android-certs/releasekey.key))
+BOARD_AVB_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_KEY_PATH := .android-certs/releasekey.key
+else
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+endif
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
